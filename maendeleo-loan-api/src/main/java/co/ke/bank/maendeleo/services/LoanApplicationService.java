948239fb3e00f3ef49605a074bb8f6a922d9ed26
kevin <kevin@localhost>
2019-02-08 17:07:29 +0300

    added comments

--- maendeleo-loan-api/src/main/java/co/ke/bank/maendeleo/services/LoanApplicationService.java
+++ maendeleo-loan-api/src/main/java/co/ke/bank/maendeleo/services/LoanApplicationService.java
@@ -8,12 +8,32 @@ import co.ke.bank.maendeleo.pojos.Response;
 
 public interface LoanApplicationService {
 	
+	/**
+	 * Creates a loan application and saves it to database
+	 * @param ApplicationRequest
+	 * @return Response
+	 */
 	public Response create(ApplicationRequest application);
 
+	/**
+	 * Retrieves all the loan applications made by a member
+	 * @param memberIdNo - this is the id or passport number
+	 * @return List<LoanApplication>
+	 */
 	public List<LoanApplication> read(int memberIdNo);
 	
+	/**
+	 * Updates the status of the application
+	 * @param LoanApplication
+	 * @return Response
+	 */
 	public Response update(LoanApplication application);
 	
+	/**
+	 * Deletes a loan application
+	 * @param loanId
+	 * @return Response
+	 */
 	public Response delete(Long loanId);
 
 }
